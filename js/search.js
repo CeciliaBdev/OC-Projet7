0a49a76ca9e193a1b48e6b825a93c6338baf8f36
CeciliaBdev <cecilia.bernard@live.fr>
2022-07-19 21:27:13 +0200

    affichage recette avec la recherche via l'input ok

--- js/search.js
+++ js/search.js
@@ -1,9 +1,12 @@
 import { recipesData } from '../data/recipes.js'
+import { CardRecipes } from '../js/RecipesCard.js'
 
 // selectionne l'input
 const inputSearch = document.querySelector('.container input')
+const placeRecipe = document.querySelector('#card_container')
 
 console.log(recipesData)
+
 // evenement : recupere en temps réel le champ input (tapé au clavier)
 inputSearch.addEventListener('keyup', () => {
   const resultat = inputSearch.value
@@ -19,11 +22,41 @@ inputSearch.addEventListener('keyup', () => {
       //   if (el.name.includes('Salade')) {
       //     return el
       //   }
-      if (el.name.toLocaleLowerCase().includes(String(resultat))) return el
+      if (el.name.toLocaleLowerCase().includes(String(resultat))) {
+        placeRecipe.innerHTML = ''
+        return el
+      }
     })
     // tableau filtré
     console.log(tabFilter)
+
+    // affichage recette
+    class MainApp {
+      constructor() {
+        this.articleCardContainer = document.querySelector('#card_container')
+        this.Recipes = tabFilter
+        //console.log(recipesData) //mon tableau de recette
+      }
+
+      displayCardRecipes() {
+        const tabFilter = this.Recipes
+        tabFilter.forEach((element) => {
+          const template = new CardRecipes(element)
+
+          const card = document.createElement('article')
+          card.classList.add('d-inline-block', 'col-4', 'mt-5')
+          card.innerHTML = template.createCard()
+
+          this.articleCardContainer.appendChild(card)
+        })
+      }
+
+      init() {
+        this.displayCardRecipes()
+      }
+    }
+
+    const app = new MainApp()
+    app.init()
   }
 })
-
-// test filtre
